992e1d7cd33ab2ebcd11116804b1e95daf6df24a
yashaka <yashaka@gmail.com>
2022-09-13 17:21:39 +0300

    add typings for selene Element extensions
    - to activate them in PyCharm: mark the typings directory as "sources root" (via context click and select the corresponding menu option)
      - but the autocomplete feature will not work in PyCharm because of the bug to be fixed (see https://youtrack.jetbrains.com/issue/PY-56217/Partial-stubs-are-not-handled-properly)

---
+++ typings/selene/core/entity.pyi
@@ -0,0 +1,6 @@
+from selene.core.entity import WaitingEntity
+
+class Element(WaitingEntity):
+    def long_press(self: Element, duration=1.0) -> Element: ...
+
+    def tap(self: Element) -> Element: ...
